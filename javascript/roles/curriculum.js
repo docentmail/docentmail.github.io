c4d7e03ddd02c91bc1a507d4294e779a8ce67480
Henadzi Kastyshyn <docentmail@gmail.com>
2016-02-27 00:18:40 -0800

    "JavaScript for Novice"  was added. Standards and versions support. - for Novices was added

---
+++ javascript/roles/curriculum.js
@@ -0,0 +1,20 @@
+---
+layout: javascript-file
+permalink: /javascript/roles/curriculum.js
+---
+
+/*
+
+
+ca6243a4-7270-496b-ae09-91d0edd5de44
+*/
+var curriculum=[
+    {"id": "2cd18723-46ec-48c1-a79b-60e5fd361ec5",
+     "title":"JavaScript for Java developers"
+    },
+    {"id": "b0b97966-e5d0-4863-9870-5a70971c15a7",
+     "title":"JavaDScript for novice"
+    }
+];
+
+
